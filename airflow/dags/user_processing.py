d09b41b78d1d76b46b39158a39c767525aefb868
Ulises Olvera <ulises.olvera@flyrlabs.com>
2022-10-06 22:45:41 -0500

    first dag

--- airflow/dags/user_processing.py
+++ airflow/dags/user_processing.py
@@ -1,1 +1,6 @@
+import imp
+from multiprocessing.spawn import import_main_path
+from airflow import DAG
+from datetime import datetime
 
+with DAG('user_processing', start_date=datetime(2022,10,6),         schedule_interval='@daily', catchup=False)
