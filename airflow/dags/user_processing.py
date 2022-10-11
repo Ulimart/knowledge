8b3dad312be3f7b3c07f77ddda5726f996228497
Ulises Olvera <ulises.olvera@flyrlabs.com>
2022-10-10 19:56:38 -0500

    table created

--- airflow/dags/user_processing.py
+++ airflow/dags/user_processing.py
@@ -1,6 +1,22 @@
 import imp
 from multiprocessing.spawn import import_main_path
+from venv import create
 from airflow import DAG
 from datetime import datetime
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+with DAG('user_processing', start_date=datetime(2022,10,6),         schedule_interval='@daily', catchup=False) as dag:
 
-with DAG('user_processing', start_date=datetime(2022,10,6),         schedule_interval='@daily', catchup=False)
+    create_table = PostgresOperator(
+        task_id='create_table',
+        postgres_conn_id='postgress',
+        sql='''
+        CREATE TABLE ID NOT EXISTS users (
+            firstname TEXT NOT NULL,
+            lastname TEXT NOT NULL,
+            country TEXT NOT NULL,
+            username TEXT NOT NULL,
+            password TEXT NOT NULL,
+            email TEXT NOT NULL
+        )
+        '''
+    )
