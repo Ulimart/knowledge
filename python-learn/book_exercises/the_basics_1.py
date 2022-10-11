d8bf872e6a3e05141232d9996ed591967849e7c1
Ulises Olvera <ulises.olvera@flyrlabs.com>
2022-10-10 20:33:47 -0500

    add exercise_4 and all the next excercises

--- python-learn/book_exercises/the_basics_1.py
+++ python-learn/book_exercises/the_basics_1.py
@@ -24,3 +24,62 @@ create it using only one line of code.
 
 #print("What do you call a bear with no teeth? " +  "\n A gummy bear!")
 
+"""
+Ask the user to enter two numbers. Add them together and display the answer as
+The toal is [Answer]
+"""
+
+# num_1 = int(input("Add the first value: "))
+# num_2 = int(input("Add the second value: "))
+# sum = str(num_1 + num_2)
+# print("The total is: " + sum)
+
+"""
+Ask the user to enter three
+numbers. Add together the first
+two numbers and then multiply
+this total by the third. Display the
+answer as The answer is
+[answer]
+"""
+"""Ask how many slices
+of pizza the user
+started with and ask
+how many slices
+they have eaten.
+Work out how many
+slices they have left
+and display the
+answer in a user-
+friendly format.
+"""
+"""Ask the user for their name and their age. Add 1 to their age
+and display the output [Name] next birthday you
+will be [new age] .
+"""
+"""
+Ask for the total price of the bill, then ask how
+many diners there are. Divide the total bill by the
+number of diners and show how much each
+person must pay.
+"""
+"""
+Write a program
+that will ask for a
+number of days
+and then will
+show how many
+hours, minutes
+and seconds are
+in that number of
+days.
+"""
+"""There are 2,204 pounds in a kilogram. Ask the
+user to enter a weight in kilograms and convert it
+to pounds.
+"""
+"""
+Task the user to enter a number over 100 and then enter a number under
+10 and tell them how many times the smaller number goes into the larger
+number in a user-friendly format.
+"""
