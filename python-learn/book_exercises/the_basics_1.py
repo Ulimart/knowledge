f7d75dcf60b4cfce0ecf0985dc0336b087507363
Ulises Olvera <ulises.olvera@flyrlabs.com>
2022-10-10 20:19:48 -0500

    exercise_3

--- python-learn/book_exercises/the_basics_1.py
+++ python-learn/book_exercises/the_basics_1.py
@@ -1,6 +1,7 @@
 """Ask for the user’s first name and
 display the output message
-Hello [First Name] ."""
+Hello [First Name] 
+"""
 
 # nombre = input("What is yor first name? ")
 # print(nombre)
@@ -8,9 +9,18 @@ Hello [First Name] ."""
 """
 Ask for the user’s first name and then ask for
 their surname and display the output message
-Hello [First Name] [Surname] .
+Hello [First Name] [Surname]
 """
 
 # name = input("What is your first name? ")
 # last_name = input("What is your lastname? ")
 # print("Hello " + name + " " + last_name)
+
+"""
+Write code that will display the joke “What do you call a bear with no
+teeth?” and on the next line display the answer “A gummy bear!” Try to
+create it using only one line of code.
+"""
+
+#print("What do you call a bear with no teeth? " +  "\n A gummy bear!")
+
