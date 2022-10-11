98a3ec23c9fd5072bcf9085ccd840d13525721d4
Ulises Olvera <ulises.olvera@flyrlabs.com>
2022-10-10 20:14:15 -0500

    exercise_2

--- python-learn/book_exercises/the_basics_1.py
+++ python-learn/book_exercises/the_basics_1.py
@@ -2,5 +2,15 @@
 display the output message
 Hello [First Name] ."""
 
-nombre = input("What is yor first name? ")
-print(nombre)
+# nombre = input("What is yor first name? ")
+# print(nombre)
+
+"""
+Ask for the user’s first name and then ask for
+their surname and display the output message
+Hello [First Name] [Surname] .
+"""
+
+# name = input("What is your first name? ")
+# last_name = input("What is your lastname? ")
+# print("Hello " + name + " " + last_name)
